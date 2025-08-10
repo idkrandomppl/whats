a281824d943a0e280df464948b32b04392d22445
idkrandomppl <129311508+idkrandomppl@users.noreply.github.com>
2025-08-09 23:10:57 -0400

    Create App.tsx

---
+++ client/src/components/App.tsx
@@ -0,0 +1,30 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/hooks/use-theme";
+import Home from "@/pages/home";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="light" storageKey="discord-timer-theme">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
